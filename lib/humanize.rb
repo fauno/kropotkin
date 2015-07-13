e699c3cf5ff0ace190c35a364976df86d3d20238
Nicolás Reynolds <fauno@endefensadelsl.org>
2015-07-13 19:02:51 -0300

    algunas cosas las queremos decir inmediatamente

--- lib/humanize.rb
+++ lib/humanize.rb
@@ -3,10 +3,14 @@ require 'cinch/timer'
 # Agregar un temporizador para que las respuestas no sean inmediatas y
 # parezca más humano
 module HumanMessage
-  def reply(text, prefix = false)
-    Thread.new do
-      sleep rand(10)
-      super
+  def reply(text, prefix = false, now = false)
+    if now
+      super(text, prefix)
+    else
+      Thread.new do
+        sleep rand(10)
+        super(text, prefix)
+      end
     end
   end
 end
